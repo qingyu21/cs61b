17f8e0d1cfd5343f6936394f813e78294c2080d0
Qingyu Yin <853623491@qq.com>
2021-08-02 23:35:19 +0800

    complete animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -26,4 +26,45 @@ public class NBody {
 
         return planets;
     }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+
+        double radius = readRadius(filename);
+        Planet[] planets = readPlanets(filename);
+
+        StdDraw.enableDoubleBuffering();
+
+        // set scale
+        StdDraw.setScale(-radius, radius);
+
+        StdDraw.clear();
+
+        double t = 0.0;
+        while (t <= T) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int i = 0; i < planets.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+
+            // draw
+            StdDraw.picture(0, 0, "./images/starfield.jpg", radius * 2, radius * 2);
+            for (Planet planet : planets) {
+                String filepath = "./images/" + planet.imgFileName;
+                StdDraw.picture(planet.xxPos, planet.yyPos, filepath);
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            t += dt;
+        }
+
+    }
+
 }
