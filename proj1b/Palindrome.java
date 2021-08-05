a431104323787d34fef0bb21100e4095a75f8402
Qingyu Yin <853623491@qq.com>
2021-08-05 13:55:35 +0800

    implemented isPalindrome and passed test

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -8,6 +8,18 @@ public class Palindrome {
     }
 
     public boolean isPalindrome(String word) {
-        return false;
+        if (word == null || word.length() == 0 || word.length() == 1) {
+            return true;
+        }
+
+        Deque<Character> deque = wordToDeque(word);
+        while (deque.size() > 1) {
+            Character front = deque.removeFirst();
+            Character last = deque.removeLast();
+            if (!front.equals(last)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
